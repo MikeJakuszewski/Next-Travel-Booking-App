60b51cc03b1be45fb7fc10f6ce921a9890d85fb2
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-09-20 17:28:34 -0400

    added google oauth functionality

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import bcrypt from "bcrypt";
 import prisma from "@/app/libs/prismadb";
 import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: AuthOptions = {
