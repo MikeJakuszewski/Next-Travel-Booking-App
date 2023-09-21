6c7b9f47629d02a503da13833f62c7e0d33fd844
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-09-21 14:51:20 -0400

    added categories, added register and login toggle

--- app/components/Modal/LoginModal.tsx
+++ app/components/Modal/LoginModal.tsx
@@ -51,6 +51,11 @@ const LoginModal = () => {
     });
   };
 
+  const toggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
+
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome back" subtitle="Login to your account!" center />
@@ -91,12 +96,12 @@ const LoginModal = () => {
       />
       <div className=" text-neutral-500 mt-4 font-light ">
         <div className=" flex justify-center flex-row items-center gap-2">
-          <div>Already have an account?</div>
+          <div>First time using Airbnb?</div>
           <div
             className="text-neutral-800 cursor-pointer hover:underline"
-            onClick={registerModal.onClose}
+            onClick={toggle}
           >
-            Log in
+            Create an account.
           </div>
         </div>
       </div>
